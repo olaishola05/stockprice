22b64b0128a3576ca627dbdecfdb3b62654e11fe
Ola Ishola <45001916+netman5@users.noreply.github.com>
2022-03-07 23:09:09 +0100

    Fetch data and add spinner

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,33 +1,46 @@
 import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { BallTriangle } from 'react-loader-spinner';
 import { getStocksFromAPI } from '../redux/stocks/stocks';
 
 const Home = () => {
+  const dispatch = useDispatch();
   const state = useSelector((state) => state.stocks);
-
-  const data = [
-    { id: 1, name: 'APPL' },
-    { id: 4, name: 'Google' },
-    { id: 3, name: 'Meta' },
-  ];
+  const { data: stocks, loading } = state;
 
   useEffect(() => {
-    getStocksFromAPI();
+    if (stocks.length === 0) {
+      dispatch(getStocksFromAPI());
+    }
   }, []);
 
-  console.log(state);
   return (
     <div>
       <h1>Hello</h1>
       <p>lorem</p>
-      <ul className="test">
-        {data.map((item) => (
-          <li key={item.id}>
-            <Link to="details">{item.name}</Link>
-          </li>
-        ))}
-      </ul>
+      {loading ? (
+        <div
+          style={{
+            width: '100%',
+            height: '100',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          {' '}
+          <BallTriangle color="#2BAD60" height="80" width="80" timeout={300} />
+        </div>
+      ) : (
+        <ul className="test">
+          {stocks.map((item) => (
+            <li key={item.symbol}>
+              <Link to="details">{item.symbol}</Link>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
--- src/redux/stocks/stocks.js
+++ src/redux/stocks/stocks.js
@@ -4,36 +4,54 @@ const FETCH_STOCKS_SUCCESS = 'stock-price/stocks/FETCH_STOCKS_SUCCESS';
 const FETCH_STOCKS_FAIL = 'stock-price/stocks/FETCH_STOCKS_FAIL';
 const API_KEY = '3c4ce1f94bdb6914b93cb87ce827213b';
 const BASE_URL = `https://financialmodelingprep.com/api/v3/stock-screener?limit=100&apikey=${API_KEY}`;
+const FETCH_START = 'stock-price/stocks/FETCH_START';
 
-const initialState = [];
+const initialState = {
+  error: false,
+  data: [],
+  loading: false,
+};
 
 const fetchStocks = (payload) => ({
   type: FETCH_STOCKS_SUCCESS,
   payload,
 });
 
-const fetchStocksFail = (payload) => ({
+const fetchStocksFail = (error) => ({
   type: FETCH_STOCKS_FAIL,
-  payload,
+  error,
+});
+
+const fetchState = () => ({
+  type: FETCH_START,
+});
+
+export const stopLoader = () => ({
+  type: 'STOP_LOADING',
 });
 
 export const getStocksFromAPI = () => async (dispatch) => {
+  dispatch(fetchState());
   try {
-    const response = await axios({
-      method: 'get',
-      url: BASE_URL,
-    });
-    console.log(response);
+    const response = await axios(BASE_URL);
+    dispatch(stopLoader());
     dispatch(fetchStocks(response.data));
   } catch (error) {
-    dispatch(fetchStocksFail([]));
+    dispatch(fetchStocksFail(error));
   }
 };
 
 const stockPriceReducer = (state = initialState, action) => {
   switch (action.type) {
+    case FETCH_START:
+      return { ...state, loading: !state.loading };
+
     case FETCH_STOCKS_SUCCESS:
-      return [...action.payload];
+      return { ...state, data: [...action.payload] };
+
+    case 'STOP_LOADING':
+      return { ...state, loading: !state.loading };
+
     case FETCH_STOCKS_FAIL:
       return [];
 
