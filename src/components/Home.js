b0da7ba892bffd1776faaeadab2a3e7d4e99ad61
Ola Ishola <45001916+netman5@users.noreply.github.com>
2022-03-08 11:22:53 +0100

    Categorizing stocks by country

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,24 @@
+/* eslint-disable implicit-arrow-linebreak */
 import React, { useEffect } from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
+// import { BsArrowRightCircle } from 'react-icons/bs';
 import { BallTriangle } from 'react-loader-spinner';
-import { getStocksFromAPI } from '../redux/stocks/stocks';
+import screener from '../screener.jpg';
+import {
+  getStocksFromAPI,
+  brazil,
+  britain,
+  taiwan,
+  china,
+  AU,
+  NL,
+  canada,
+  ireland,
+  switz,
+  france,
+  america,
+} from '../redux/stocks/stocks';
 
 const Home = () => {
   const dispatch = useDispatch();
@@ -16,29 +32,40 @@ const Home = () => {
   }, []);
 
   return (
-    <div>
-      <h1>Hello</h1>
-      <p>lorem</p>
-      <div
-        style={{
-          width: '100%',
-          height: '100',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        {' '}
-        <BallTriangle color="#2BAD60" height="80" width="80" visible={loading} />
+    <div className="app-container">
+      <h1>Stock Screener</h1>
+      <img src={screener} alt="display background" />
+      <div>
+        {loading ? (
+          <div
+            style={{
+              width: '100%',
+              height: '100',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              margin: '10rem auto',
+            }}
+          >
+            {' '}
+            <BallTriangle color="#2BAD60" height="80" width="80" />
+          </div>
+        ) : (
+          <ul className="test">
+            <li>{america.length}</li>
+            <li>{brazil.length}</li>
+            <li>{AU.length}</li>
+            <li>{NL.length}</li>
+            <li>{britain.length}</li>
+            <li>{taiwan.length}</li>
+            <li>{china.length}</li>
+            <li>{canada.length}</li>
+            <li>{ireland.length}</li>
+            <li>{france.length}</li>
+            <li>{switz.length}</li>
+          </ul>
+        )}
       </div>
-
-      <ul className="test">
-        {stocks.map((item) => (
-          <li key={item.symbol}>
-            <Link to="details">{item.symbol}</Link>
-          </li>
-        ))}
-      </ul>
     </div>
   );
 };
